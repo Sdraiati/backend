7a6e6257dcdd10fe1a647548db64ef35ac28d73e
bassupreme <leonardo.basso.7@studenti.unipd.it>
2024-01-29 18:51:44 +0100

    Testata la rimozione di eliminazione da parte di un utente di un progetto.

--- api/progetto/elimina_progetto.php
+++ api/progetto/elimina_progetto.php
@@ -5,7 +5,7 @@
 <?php
 
 // prendere i valori dalla POST request 
-$email = "leonardo.basso02@gmail.com";
+$email = "eghosa.basso07@gmail.com" ;
 $hash_email = sha1($email);
 $id_progetto = "10";
 
@@ -21,21 +21,32 @@ $sql_count = "SELECT COUNT(id_progetto) FROM progetto_utente WHERE id_progetto =
 
 echo '<h1> ' . $sql_delete_spese . ' </h1>';
 echo '<h1> ' . $sql_delete_legame . ' </h1>';
+echo '<h1> ' . $sql_count . ' </h1>';
 
-if ($sql_count == 0) {
+$result = mysqli_query($conn, $sql_delete_spese);
+if ($result) {
+    echo '<h2> spese rimosse </h2>';
+} 
+
+$result_legame = mysqli_query($conn, $sql_delete_legame);
+if ($result_legame) {
+    echo '<h2> legame rimosso </h2>';
+} 
+
+$result_count = mysqli_query($conn, $sql_count);
+$array = mysqli_fetch_all($result_count, MYSQLI_ASSOC); // array associativo
+$n_users_left = intval($array[0]['COUNT(id_progetto)']);
+
+if ($n_users_left == 0) {
     // eseguire sql_delete_project
     $sql_delete_project = "DELETE FROM progetto WHERE id = ${id_progetto}; ";
-}
-
-
-/*
-$result = mysqli_query($conn, $sql);
-if ($result) {
-    echo '<h2> transazione riuscita </h2>';
+    $result_delete = mysqli_query($conn, $sql_delete_project);
+    if ($result_delete) {
+        echo '<h2> progetto rimosso </h2>';
+    }
 } else {
-    echo '<h2> transazione NON riuscita </h2>';
+    echo '<h2> ci sono ancora utenti legati al progetto. </h2>';
 }
-*/
 
 if($conn->close()) {
     echo '<h2> connection closed </h2>';

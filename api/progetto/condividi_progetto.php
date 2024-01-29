f873e4b6eeb8211608659fd7394f91d1c94e5e75
bassupreme <leonardo.basso.7@studenti.unipd.it>
2024-01-29 12:28:23 +0100

    Aggiunte informazioni di debug all'endpoint condivisione del progetto.

--- api/progetto/condividi_progetto.php
+++ api/progetto/condividi_progetto.php
@@ -7,11 +7,14 @@
         logica per creare hash di condivisione gruppo
     */
 
-$id_progetto = "";
+$id_progetto = "1";
 $date_time = date("Y-m-d H:i:s");
 $link_condivisione = sha1($id_progetto . $date_time);
 
-$sql = "UPDATE progetto SET link_condivisione = \"${link_condivisione}\" WHERE id = ${id_progetto};"
-$link = "http://path_to_website/shared_project?=${link_condivisione}";
+$sql = "UPDATE progetto SET link_condivisione = \"${link_condivisione}\" WHERE id = ${id_progetto};";
+$link = "http://path_to_website/shared_project?=\"${link_condivisione}\"";
+
+echo "<h1> " . $sql . "</h1>";
+echo "<h1> " . $link . "</h1>";
 
 // ritornare il link di condivisione oppure la pagina?

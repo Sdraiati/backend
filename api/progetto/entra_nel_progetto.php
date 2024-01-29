4817456a41c10c62a258aee2ff81d21e88d235c2
bassupreme <leonardo.basso.7@studenti.unipd.it>
2024-01-29 15:52:20 +0100

    inserita la logica per fa entrare un utente all'interno di un progetto

--- api/progetto/entra_nel_progetto.php
+++ api/progetto/entra_nel_progetto.php
@@ -4,18 +4,33 @@
 
 <?php
 
-$link = $_GET["link_condivisione"];
+$user_email = $_SESSION['email']; // l'email dell'utente all'interno del progetto sono salvate all'interno salvate all'interno di 
+$hash = $_GET["shared_project"];
+$sql = "SELECT id, link_condivisione FROM progetto WHERE link_condivisione = \"${hash}\"; ";
 
-$sql = "SELECT id FROM progetto WHERE link_condivisione = \"${link}\"; ";
-
-/*
 $result = mysqli_query($conn, $sql);
-if ($result) {
-    $array = mysqli_fetch_all($result, MYSQLI_ASSOC); // array associativo
-    if (count($array) > 0) {
-        var_dump($array);
+if ($result) { // quindi il progetto esiste.
+    // inserire l'utente all'interno del progetto. 
+    // => inserire associazione all'interno della tabella progetto_utente.
+    $array = mysqli_fetch_all($result, MYSQLI_ASSOC);
+    $id_project = $array['id'];
+
+    
+    $insert_user = "INSERT INTO progetto_utente (email, id_progetto) VALUES (\"${user_email}\", ${id_project}); ";
+    $insert_result = mysqli_query($conn, $insert_user);
+
+    if ($insert_result) {
+        // ritornare la pagina del progetto con la disponibilitá di fare
+        // delle modifiche all'interno del progetto
+        echo '<h2> pagina del progetto <h2>'
     } else {
-        echo '<h2> non sono stati trovati movimenti associati al progetto </h2>';
+        echo '<h2> L\'utente esiste è giá inserito all\'interno del progetto. <h2>'
     }
-} 
-*/
+    
+} else {
+    echo '<h2> Il link di condivisione è scaduto...  </h2>';
+}
+
+if($conn->close()) {
+    echo '<h2> connection closed </h2>';
+}

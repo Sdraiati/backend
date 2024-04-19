334098dc051a7b03a4c3e161d87c8cccf8523ec2
simocare <simone.caregnato@studenti.unipd.it>
2024-04-19 19:29:19 +0200

    classe Database

--- api/config/database.php
+++ api/config/database.php
@@ -1,15 +1,43 @@
 <?php
-// create connection
-function connect() {
-	$DB_HOST = 'localhost';
-	$DB_USER = 'root';
-	$DB_PASS = '';
-	$DB_NAME = 'penny_wise_db';
+class Database {
+    private $host;
+    private $db_name;
+    private $username;
+    private $password;
+    private $conn;
+    // Singleton pattern
+    private static $instance = null;
 
-	$conn = new mysqli($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME);
-	if ($conn->connect_error) {
-		die("Connection failed");
-	}
-	return $conn;
+    private function __construct($host, $db_name, $username, $password) {
+        $this->host = $host;
+        $this->db_name = $db_name;
+        $this->username = $username;
+        $this->password = $password;
+        $this->conn = new mysqli($host, $username, $password, $db_name);
+
+        if ($this->conn->connect_error) {
+            die("Connection failed: " . $this->conn->connect_error);
+        }
+    }
+
+    public static function getInstance($host, $db_name, $username, $password) {
+        // check if the instance is already created
+        if (self::$instance == null) {
+            try {
+                self::$instance = new Database($host, $db_name, $username, $password);
+            } catch (Exception $e) {
+                die("get instance error: " . $e->getMessage());
+            }
+        }
+        return self::$instance;
+    }
+
+    public function query($sql) {
+        $result = $this->conn->query($sql);
+        if ($result === false) {
+            die("query error: " . $this->conn->error);
+        }
+        return $result;
+    }
 }
 ?>
---
+++ api/config/db_config.php
@@ -0,0 +1,6 @@
+<?php
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'penny_wise_db');
+define('DB_USERNAME', 'simone');
+define('DB_PASSWORD', 'user');
+?>

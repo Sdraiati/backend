ce03429320e701a18b2dfa327aa387300b568888
simocare <simone.caregnato@studenti.unipd.it>
2024-04-24 22:53:53 +0200

    JoinProject e fix attributi Project

---
+++ models/database/project/JoinProject.php
@@ -0,0 +1,31 @@
+<?php
+require_once ('Project.php');
+class JoinProject extends Project {
+    protected UserInfo $userInfo;
+    protected ProjectInfo $projectInfo;
+    public function __construct(Database $db) {
+        parent::__construct($db);
+        $this->userInfo = new UserInfo($db);
+        $this->projectInfo = new ProjectInfo($db);
+    }
+
+    public function joinProject($email, $project_id) : bool {
+        // check if the user exists
+        if (!$this->userInfo->exists($email))
+            die("joinProject: user not found with email $email");
+
+        // check if the project exists
+        if (!$this->projectInfo->exists($project_id))
+            die("joinProject: project not found with id $project_id");
+
+        $sql = "INSERT INTO progetto_utente (id_progetto, email) VALUES (?, ?)";
+        $params = [
+            ['type' => 'i', 'value' => $project_id],
+            ['type' => 's', 'value' => $email]
+        ];
+        $stmt = $this->db->prepareAndBindParams($sql, $params);
+        $stmt->execute() or die($stmt->error);
+
+        return true;
+    }
+}
--- models/database/project/Project.php
+++ models/database/project/Project.php
@@ -6,10 +6,8 @@ require_once (__PROJECTROOT__.'/models/database/user/UserInfo.php');
 
 class Project {
     protected Database $db;
-    protected UserInfo $userInfo;
 
     protected function __construct(Database $db) {
         $this->db = $db;
-        $this->userInfo = new UserInfo($db);
     }
 }

8aa0a3d9bc48f836acd222e9e716f64af09be9d2
simocare <simone.caregnato@studenti.unipd.it>
2024-05-09 00:16:22 +0200

    test htaccess

--- index.php
+++ index.php
@@ -5,16 +5,18 @@ include "scripts/generate_header.php";
 
 $coso = new Controller();
 
+#print $_SERVER['REQUEST_URI'];
 
-if($_SERVER['REQUEST_METHOD'] == "GET" && $_SERVER['REQUEST_URI'] == "/backend/index.php")
+if($_SERVER['REQUEST_METHOD'] == "GET" && $_SERVER['REQUEST_URI'] == "/index.php")
 {
     $html_content = file_get_contents('views/index.html');
     $header = generate_header("index");
     $html_content = $header . $html_content;
     $coso->renderPage($html_content);
 }
-else if($_SERVER['REQUEST_METHOD'] == "GET" && $_SERVER['REQUEST_URI'] == "/backend/about_us")
+else if($_SERVER['REQUEST_METHOD'] == "GET" && $_SERVER['REQUEST_URI'] == "/about_us")
 {
+    print $_SERVER['REQUEST_URI'];
     $coso->renderAboutUsPage();
 }
 else{

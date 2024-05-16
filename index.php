30b4256a639264147077007ae2d3dac5604f62d9
eghosa02 <pezzoloedoardo@gmail.com>
2024-05-17 00:15:43 +0200

    modificato files html, php

--- index.php
+++ index.php
@@ -1,10 +1,5 @@
 <?php
 
-//require_once("controllers/Controller.php");
-//require_once("models/database/project/NewProject.php");
-//require_once("models/database/user/NewUser.php");
-//require_once("api/config/database.php");
-//require_once("api/config/db_config.php");
 require_once("routes.php");
 
 function formatUrl($stringa) {
@@ -18,8 +13,6 @@ function formatUrl($stringa) {
 }
 
 session_start();
-//$database = Database::getInstance(DB_HOST, DB_NAME, DB_USERNAME, DB_PASSWORD);
-//$controller = new Controller();
 
 $logged = isset($_COOKIE["logIn"]);
 if($logged) {$_SESSION["LogIn"] = $_COOKIE["LogIn"];}
@@ -36,47 +29,3 @@ if(array_key_exists($url, $routes))
 else{
     $controller->renderPage("resource_not_found", $logged); 
 }
-
-
-
-/*
-if($_SERVER['REQUEST_METHOD'] == "GET")
-{
-    $logged = isset($_COOKIE["logIn"]);
-    if($logged) {$_SESSION["LogIn"] = $_COOKIE["LogIn"];}
-    try{
-        $controller->renderPage(formatUrl($_SERVER['REQUEST_URI']), $logged);
-    }catch (Exception $e) {
-        $controller->renderPage("resource_not_found", $logged); 
-    }  
-}
-else if($_SERVER['REQUEST_METHOD'] == "POST")
-{
-    if($_SERVER['REQUEST_URI']=="/account_home")
-    {
-        if(isset($_SESSION["LogIn"]))
-        {
-            $nome = $_POST["nomeProgetto"];
-            $descrizione = $_POST["descrizioneProgetto"];
-            $email = $_SESSION["LogIn"]["email"];
-            $link_condivisione = 'bho';
-            $newProject = new NewProject($database);
-            $newProject->createProject($email, $nome, $link_condivisione, $descrizione);
-            header("Location: /account_home");
-        }
-        else{
-            $controller->renderPage(formatUrl($_SERVER['REQUEST_URI']), isset($_COOKIE["logIn"]));
-        }
-    }
-    else if($_SERVER['REQUEST_URI']=="/registration")
-    {
-        $newUser = new NewUser($database);
-        $newUser->createUser($_POST['email'], $_POST['username'], $_POST['password']);
-        $_COOKIE["LogIn"] = ["email"=>$_POST['email'], "username"=>$_POST['username'], "password"=>$_POST['password']];
-        $_SESSION["LogIn"] = ["email"=>$_POST['email'], "username"=>$_POST['username'], "password"=>$_POST['password']];
-        header("Location: /account_home");
-    }
-}
-else{
-    $controller->renderPage("resource_not_found", isset($_COOKIE["logIn"]));
-}*/
--- routes.php
+++ routes.php
@@ -1,60 +1,6 @@
 <?php
 
-require_once("controllers/Controller.php");
-require_once("api/config/database.php");
-require_once("api/config/db_config.php");
-require_once("models/database/project/NewProject.php");
-require_once("models/database/user/NewUser.php");
-
-
-$database = Database::getInstance(DB_HOST, DB_NAME, DB_USERNAME, DB_PASSWORD);
-$controller = new Controller();
-
-function generalPage($_, $logged)
-{
-    global $controller;
-    $controller->renderPage(formatUrl($_SERVER['REQUEST_URI']), $logged);
-}
-
-function registration($method, $logged)
-{
-    global $controller;
-    global $database;
-    if($method == "POST")
-    {
-        $newUser = new NewUser($database);
-        $newUser->createUser($_POST['email'], $_POST['username'], $_POST['password']);
-        $_COOKIE["LogIn"] = ["email"=>$_POST['email'], "username"=>$_POST['username'], "password"=>$_POST['password']];
-        $_SESSION["LogIn"] = ["email"=>$_POST['email'], "username"=>$_POST['username'], "password"=>$_POST['password']];
-        header("Location: /account_home");
-    }
-}
-function account_home($method, $logged)
-{
-    global $controller;
-    global $database;
-    if($method == "POST")
-    {
-        if(isset($_SESSION["LogIn"]))
-        {
-            $nome = $_POST["nomeProgetto"];
-            $descrizione = $_POST["descrizioneProgetto"];
-            $email = $_SESSION["LogIn"]["email"];
-            $link_condivisione = 'bho2';
-            $newProject = new NewProject($database);
-            $newProject->createProject($email, $nome, $link_condivisione, $descrizione);
-            header("Location: /account_home");
-        }
-        else{
-            $controller->renderPage(formatUrl($_SERVER['REQUEST_URI']), isset($_COOKIE["logIn"]));
-        }
-    }
-    else if($method == "GET")
-    {
-        //lista progetti
-        $controller->renderPage(formatUrl($_SERVER['REQUEST_URI']), $logged);
-    }
-}
+require_once("direct.php");
 
 $routes = array(
     "/about_us" => function($method, $logged) {

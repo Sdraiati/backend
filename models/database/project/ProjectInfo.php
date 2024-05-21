d53c94487be551713946cc64faa8ef14d61158f4
simocare <simone.caregnato@studenti.unipd.it>
2024-05-21 23:20:45 +0200

    aggiunto funzione che restituisce i dati di un progetto e funzione che restituisce lista di progetti nella classe ProjectInfo

--- models/database/project/ProjectInfo.php
+++ models/database/project/ProjectInfo.php
@@ -1,6 +1,6 @@
 <?php
 define('__PROJECTINFOROOT__', dirname(__FILE__, 4));
-require_once (__PROJECTROOT__.'/models/database/DatabaseManager.php');
+require_once (__PROJECTINFOROOT__.'/models/database/DatabaseManager.php');
 class ProjectInfo extends DatabaseManager {
     public function __construct(Database $db)
     {
@@ -15,5 +15,32 @@ class ProjectInfo extends DatabaseManager {
         $stmt = $this->db->prepareAndBindParams($sql, $params);
         $stmt->execute() or die($stmt->error);
         return $stmt->get_result()->num_rows > 0;
+    }
+
+    public function getProjectInfo($projectId): array {
+        $sql = "SELECT * FROM progetto WHERE id = ?";
+        $params = [
+            ['type' => 'i', 'value' => $projectId]
+        ];
+        $stmt = $this->db->prepareAndBindParams($sql, $params);
+        $stmt->execute() or die($stmt->error);
+        return $stmt->get_result()->fetch_assoc();
+    }
+
+    public function getProjectList(): array
+    {
+        $sql_list = "SELECT id FROM progetto";
+        $stmt_list = $this->db->query($sql_list);
+        $project_list = [];
+        while ($row = $stmt_list->fetch_assoc()) {
+            $sql = "SELECT * FROM progetto WHERE id = ?";
+            $params = [
+                ['type' => 'i', 'value' => $row['id']]
+            ];
+            $stmt = $this->db->prepareAndBindParams($sql, $params);
+            $stmt->execute() or die($stmt->error);
+            $project_list[] = $stmt->get_result()->fetch_assoc();
+        }
+        return $project_list;
     }
 }

b67548754dea1842cfaec39c87d6611cf09f4589
Carlo Rosso <carlorosso@Carlos-MacBook-Air.local>
2024-05-24 20:40:19 +0200

    ho commentato l'index per far funzionare la prima api

--- index.php
+++ index.php
@@ -7,22 +7,45 @@ require_once("routes.php");
 session_start();
 
 $logged = isset($_COOKIE["LogIn"]);
-if($logged) {$_SESSION["LogIn"] = $_COOKIE["LogIn"];}
+if ($logged) {
+	$_SESSION["LogIn"] = $_COOKIE["LogIn"];
+}
 $url = formatUrl($_SERVER['REQUEST_URI']);
 
 
-if(array_key_exists($url, $routes))
-{
-    $logged = isset($_COOKIE["LogIn"]);
-    if($logged) {$_SESSION["LogIn"] = $_COOKIE["LogIn"];}
-    $fun = $routes[$url];
-    try{
-        $fun($_SERVER['REQUEST_METHOD'], $logged);
-    }
-    catch(Exception $e){
-        print $e->getMessage();
-    }
+if (array_key_exists($url, $routes)) {
+	$logged = isset($_COOKIE["LogIn"]);
+	if ($logged) {
+		$_SESSION["LogIn"] = $_COOKIE["LogIn"];
+	}
+	$fun = $routes[$url];
+	try {
+		$fun($_SERVER['REQUEST_METHOD'], $logged);
+	} catch (Exception $e) {
+		print $e->getMessage();
+	}
+} else {
+	$controller->renderPage("resource_not_found", $logged);
 }
-else{
-    $controller->renderPage("resource_not_found", $logged); 
+
+/*
+require_once("controllers/json/User.php");
+require_once("controllers/Router.php");
+
+
+
+session_start();
+
+$logged = isset($_COOKIE["LogIn"]);
+if ($logged) {
+	$_SESSION["LogIn"] = $_COOKIE["LogIn"];
 }
+
+//$url = formatUrl($_SERVER['REQUEST_URI']);
+
+$router = new Router();
+
+$router->addRoute($registerUser);
+
+$router->handle($_SERVER['REQUEST_URI'], $_SERVER['REQUEST_METHOD']);
+*/

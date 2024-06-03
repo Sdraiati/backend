779e02e1f331a5595df0d1ab7801078fb94df015
Carlo Rosso <carlorosso@Carlos-Air.lan>
2024-06-03 23:52:36 +0200

    creazione delle api dell'utente

--- controllers/json/User.php
+++ controllers/json/User.php
@@ -2,27 +2,61 @@
 
 include_once 'JsonApi.php';
 include_once __PROJECTROOT__ . '/models/database/user/NewUser.php';
+include_once __PROJECTROOT__ . '/models/database/user/ModifyUser.php';
+include_once __PROJECTROOT__ . '/api/config/db_config.php';
 
 $registerUser = (new JsonApiBuilder())
 	->setPath('/user/register')
-	->setMethod('POST')
 	->setInputParams(['email', 'username', 'password'])
-	->setLogicClass('NewUser')
-	->setLogicMethod('createUser')
-	->setErrorHandler(function ($message) {
-		http_response_code(400);
-		echo json_encode(['error' => $message]);
-	})
+	->setLogicFn(
+		function ($params) {
+			try {
+				$newUser = new NewUser(Database::getInstance(DB_HOST, DB_NAME, DB_USERNAME, DB_PASSWORD));
+				$newUser->createUser($params['email'], $params['username'], $params['password']);
+			} catch (Exception $_) {
+				header('Content-Type: application/json');
+				echo json_encode(['error' => "User already exists"]);
+			}
+		}
+	)
 	->createApi();
 
 $loginUser = (new JsonApiBuilder())
 	->setPath('/user/login')
-	->setMethod('POST')
 	->setInputParams(['email', 'password'])
-	->setLogicClass('UserInfo')
-	->setLogicMethod('exists')
-	->setErrorHandler(function ($message) {
-		http_response_code(400);
-		echo json_encode(['error' => $message]);
-	})
+	->setLogicFn(
+		function ($params) {
+			try {
+				$user = new UserInfo(Database::getInstance(DB_HOST, DB_NAME, DB_USERNAME, DB_PASSWORD));
+				if (!$user->checkCredentials($params['email'], $params['password'])) {
+					throw new Exception("Invalid credentials");
+				} else {
+					setCookieUser($params['email'], $params['username'], $params['password']);
+				}
+			} catch (Exception $_) {
+				header('Content-Type: application/json');
+				echo json_encode(['error' => "Invalid credentials"]);
+			}
+		}
+	)
+	->createApi();
+
+$modifyUser = (new JsonApiBuilder())
+	->setPath('/user/modify')
+	->setInputParams(['newEmail', 'newUsername', 'newPassword'])
+	->setLogicFn(
+		function ($params) {
+			try {
+				$user = new ModifyUser(Database::getInstance(DB_HOST, DB_NAME, DB_USERNAME, DB_PASSWORD));
+				$email = json_decode($_SESSION["LogIn"], true)["email"];
+				$password = json_decode($_SESSION["LogIn"], true)["password"];
+				$user->modify($email, $password, $params['newEmail'], $params['newUsername'], $params['newPassword']);
+				setCookieUser($params['newEmail'], $params['newUsername'], $params['newPassword']);
+				header("Location: /accout_home");
+			} catch (Exception $_) {
+				header('Content-Type: application/json');
+				echo json_encode(['error' => "Invalid credentials"]);
+			}
+		}
+	)
 	->createApi();

96e3caf9855d85a78be6cb2c723e8c642afcc0fa
simocare <simone.caregnato@studenti.unipd.it>
2024-06-13 11:49:59 +0200

    dopo logout reindirizza all'home page

--- assets/js/modifica.js
+++ assets/js/modifica.js
@@ -111,7 +111,7 @@ function logOut() {
 		document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT;path=/";
 	}
 
-	window.location.reload();
+	window.location.href = '/';
 }
 
 function share(link) {

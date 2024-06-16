f094ef3330f1d019f9a78bd43746d0ad3a650b08
simocare <simone.caregnato@studenti.unipd.it>
2024-06-16 10:39:37 +0200

    descrizione progetto in project_home

--- controllers/html/ProjectHomeHtmlApi.php
+++ controllers/html/ProjectHomeHtmlApi.php
@@ -13,7 +13,7 @@ class ProjectHomeHtmlApi extends HtmlApi
 		parent::__construct($path);
 	}
 
-	private function get_project_name($project_id)
+	private function get_project_name($project_id) : string | null
 	{
 		global $projectManager;
 		if (!$projectManager->exists($project_id)) {
@@ -22,6 +22,17 @@ class ProjectHomeHtmlApi extends HtmlApi
 		$project = $projectManager->getProjectInfo($project_id);
 		return $project['nome'];
 	}
+
+    private function get_project_description($project_id) : string | null
+    {
+        global $projectManager;
+        if (!$projectManager->exists($project_id)) {
+            return null;
+        }
+        $project = $projectManager->getProjectInfo($project_id);
+        return $project['descrizione'];
+
+    }
 
 	private function user_has_access_to_project($project_id)
 	{
@@ -54,6 +65,7 @@ class ProjectHomeHtmlApi extends HtmlApi
 		$content = $this->getContent($this->path);
 		$content = str_replace('{{ header }}', $this->getHeader(), $content);
 		$content = str_replace('{{ Project Name }}', $this->get_project_name($project_id), $content);
+        $content = str_replace('{{ Project Description }}', $this->get_project_description($project_id), $content);
 		echo $content;
 	}
 }

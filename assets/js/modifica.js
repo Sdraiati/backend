07d1ba13d77ae25df82f38af8afde33d58db9fb2
simocare <simone.caregnato@studenti.unipd.it>
2024-06-20 13:02:31 +0200

    fix

--- assets/js/modifica.js
+++ assets/js/modifica.js
@@ -458,6 +458,6 @@ function toogleView(name = "password") {
 
 function rebaseUrl(link){
 	link= link.replace('http://localhost:8080' || 'http://tecweb.studenti.math.unipd.it', '');
-	link = '/scaregna' + link;
+	link = '/scaregna/' + link;
 	return link;
 }
